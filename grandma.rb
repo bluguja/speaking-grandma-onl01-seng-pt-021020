a23038b1703bafa06df9c735df77b7bc05d4f078
Bala Luguja <bluguja@gmail.com>
2020-02-18 09:44:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,9 @@
 def speak_to_grandma(phrase) # Write a speak_to_grandma method.
 if phrase == "I LOVE YOU GRANDMA!"
   " I LOVE YOU TOO PUMPKIN!"
- 
+ elsifphrase != phrase.upcase
+ "HUH?! SPEAK UP, SONNY!"
+ else
+   "NO, NOT SINCE 1938!"
+ end
+end
